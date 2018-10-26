1147582ccbbfd4b163bbb64fc237c28d85dfd0b3
HemulGM <alinvip22@gmail.com>
2018-10-26 09:45:00 +0700

    Initial commit

---
+++ Assets/BoxScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BoxScript : MonoBehaviour {
+
+	public Animator ani;
+	public GameObject Cat;
+
+	public void OnMouseDown(){
+		if (ani.GetBool("State")) {
+			Close ();
+		}
+		else {
+			Open ();
+		};
+	}
+
+	public int Id = 0;
+	public int CatType = 0;
+
+	void Open(){
+		ani.SetBool ("State", true);
+		//gameObject.SetActive (false);
+		Cat.transform.position = new Vector3 (gameObject.transform.position.x, gameObject.transform.position.y+1.0f, gameObject.transform.position.z);
+		Cat.transform.localScale = new Vector3 (0.5f, 0.5f, 0.5f);
+		//if (Cats.cats == null) {
+		//	Cats.cats = new Cats ();
+		//}
+		//Cats.cats.SelectBox (this);
+	}
+
+	void Close(){
+		ani.SetBool ("State", false);
+		//gameObject.SetActive (true);
+		Cat.transform.position = gameObject.transform.position;
+		Cat.transform.localScale = new Vector3 (0.5f, 0.35f, 0.5f);
+	}
+
+	public void Terminate()
+	{
+		Destroy (Cat);
+		Destroy (gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		//GameObject Cat = new GameObject ("Cat");
+		string CName;
+		if (CatType == 1) {
+			CName = "Cat1";
+		} else {CName = "Cat2";
+		}
+		Cat = new GameObject ("Cat");
+		Instantiate(Cat);
+		Cat.AddComponent<SpriteRenderer>().sprite = "кот3";
+		Cat.SetActive (true);
+		Close ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Cats.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cats : MonoBehaviour {
+
+	BoxScript Box1 = null;
+	BoxScript Box2 = null;
+	public static Cats cats;
+
+	// Use this for initialization
+	void Start () {
+		cats = new Cats();
+	}
+
+	public void SelectBox(BoxScript box)
+	{
+		if (Box1 == null) { Box1 = box; return; };
+		if (Box2 == null) { Box2 = box; return; };
+		if (Box1.CatType == Box2.CatType) {
+			Box1.Terminate ();
+			Box2.Terminate ();
+			Box1 = null;
+			Box2 = null;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
